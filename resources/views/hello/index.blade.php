e28837138a02bd106c8241ed8d5e5426eafb1fbd
ShioriPeace <nkym_shiori.223@icloud.com>
2021-08-21 17:33:20 +0900

    bladeテンプレートをはじめて使ってみる

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Hello/Index</title>
+        <style>
+            body {font-size: 16pt; color: #999;}
+            h1{ font-size: 50pt;text-align: right; color: #f6f6f6;
+                margin: -20px 0px -30px 0px; letter-spacing: -4pt;}
+        </style>
+    </head>
+
+    <body>
+        <h1>Blade/Index</h1>
+        <p>{{ $msg }}</p>
+    </body>
+</html>
