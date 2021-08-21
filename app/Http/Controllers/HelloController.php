95672f5bb05f3044826b98868e4a14e15e38f530
ShioriPeace <nkym_shiori.223@icloud.com>
2021-08-21 18:18:04 +0900

    フォームを作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,11 +8,20 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         $data = [
-            'msg' => 'これはBladeを利用したサンプルです。',
-            'id' => $request->id
+            'msg' => 'なまえを入れてね！',
+        ];
+        return view('hello.index', $data);
+    }
+
+    public function post(Request $request)
+    {
+        $msg = $request ->msg;
+
+        $data = [
+            'msg' => 'こんにちは！' . $msg . 'さん！',
         ];
         return view('hello.index', $data);
     }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,6 +10,11 @@
 
     <body>
         <h1>Blade/Index</h1>
-        <p>{{ $msg }}</p>
+        <p>{{$msg}}</p>
+        <form method="POST" action="/hello">
+            @csrf
+            <input type="text" name="msg">
+            <input type="submit">
+        </form>
     </body>
 </html>
