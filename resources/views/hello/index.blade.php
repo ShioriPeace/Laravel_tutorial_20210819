f1789db942369bb6084e326fa150e98fcd044c4e
ShioriPeace <nkym_shiori.223@icloud.com>
2021-08-25 07:42:07 +0900

    breakとcontinueディレクティブを試す

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,11 +10,17 @@
 
     <body>
         <h1>Blade/Index</h1>
-        <p>foreachディレクティブの例</p>
+        <p>forディレクティブの例</p>
         <ol>
-            @foreach($data as $item)
-            <li>{{ $item }}</li>
-            @endforeach
+            @for ($i = 1; $i < 100; $i++)
+            @if ($i % 2 == 1)
+                @continue
+            @elseif ($i <= 10)
+            <li>No, {{ $i }}</li>
+            @else
+                @break
+            @endif
+            @endfor
         </ol>
     </body>
 </html>
