6a238060cc1e4397328643dd648c6ca106ff2ce1
ShioriPeace <nkym_shiori.223@icloud.com>
2021-08-25 07:57:14 +0900

    $loop変数

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,17 +10,17 @@
 
     <body>
         <h1>Blade/Index</h1>
-        <p>forディレクティブの例</p>
+        <p>&#064;forディレクティブの例</p>
         <ol>
-            @for ($i = 1; $i < 100; $i++)
-            @if ($i % 2 == 1)
-                @continue
-            @elseif ($i <= 10)
-            <li>No, {{ $i }}</li>
-            @else
-                @break
+            @foreach ($data as $item)
+            @if ($loop -> first)
+            <p>※データ一覧</p>
             @endif
-            @endfor
+            <li>No,{{ $loop -> iteration }}. {{ $item }}</li>
+            @if ($loop->last)
+            </ul><p>----- ここまで</p>
+            @endif
+            @endforeach
         </ol>
     </body>
 </html>
