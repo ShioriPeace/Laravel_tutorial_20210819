f32c6a9c2eb75748417fbe086c7512b735bb23f9
ShioriPeace <nkym_shiori.223@icloud.com>
2021-08-25 09:18:24 +0900

    sectionとyieldで、ベース・継承レイアウトについて

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,26 +1,18 @@
-<html>
-    <head>
-        <title>Hello/Index</title>
-        <style>
-            body {font-size: 16pt; color: #999;}
-            h1{ font-size: 50pt;text-align: right; color: #f6f6f6;
-                margin: -20px 0px -30px 0px; letter-spacing: -4pt;}
-        </style>
-    </head>
+<!-- layoutsのなかのhelloapp親レイアウトとして継承する -->
+@extends('layouts.helloapp')
+@section('title', 'Index')
 
-    <body>
-        <h1>Blade/Index</h1>
-        <p>&#064;whileディレクティブの例</p>
-        <ol>
-            @php
-            $counter = 0;
-            @endphp
-            @while ($counter < count($data))
-            <li>{{ $data[$counter] }}</li>
-            @php
-            $counter ++;
-            @endphp
-            @endwhile
-        </ol>
-    </body>
-</html>
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツ</p>
+    <p>必要なだけ、記述できます</p>
+@endsection
+
+@section('footer')
+
+copyright 2020 tuyano.
+@endsection
