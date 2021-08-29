43fd363784c952a85aadbdf369a0426d6c5b3d2a
ShioriPeace <nkym_shiori.223@icloud.com>
2021-08-29 15:43:41 +0900

    コンポーネントを読み込み、スロットで値を設定する

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,6 +10,16 @@
 @section('content')
     <p>ここが本文のコンテンツ</p>
     <p>必要なだけ、記述できます</p>
+
+    @component('components.message')
+        @slot('msg_title')
+        CAUTION!!!
+        @endslot
+
+        @slot('msg_content')
+        これはメッセージからの表示です
+        @endslot
+    @endcomponent
 @endsection
 
 @section('footer')
