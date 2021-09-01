8ed2a825efb008fac3184ae69f1256b45f07f1c0
ShioriPeace <nkym_shiori.223@icloud.com>
2021-09-02 07:41:24 +0900

    validationを追加

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,9 +8,16 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツなのですが.....</p>
-    <p>これは、なんと！<middleware>google.com</middleware>へのリンクです！！！</p>
-    <p>そして、こっちは、<middleware>yahoo.co.jp</middleware>へのリンクだったのです！！！</p>
+    <p>{{ $msg }}</p>
+    <form action="/hello" method="POST">
+        <table>
+            @csrf
+            <tr><th>name: </th><td><input type="text" name="name"></td></tr>
+            <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
+            <tr><th>age: </th><td><input type="text" name="age"></td></tr>
+            <tr><th></th><td><input type="submit" name="send"></td></tr>
+        </table>
+    </form>
 @endsection
 
 @section('footer')
