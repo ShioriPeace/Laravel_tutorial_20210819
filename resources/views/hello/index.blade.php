66103d114c5962b4dd0eb5a9c1a5e95f9386d645
ShioriPeace <nkym_shiori.223@icloud.com>
2021-09-02 07:58:30 +0900

    各項目で、エラーメッセージが出るようにした

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,19 +10,24 @@
 @section('content')
     <p>{{ $msg }}</p>
     @if(count($errors) > 0)
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+    <p>入力に問題あり！！再入力してください</p>
     @endif
     <form action="/hello" method="POST">
         <table>
             @csrf
+            @if($errors->has('name'))
+            <tr><th>ERRORRR</th><td>{{ $errors->first('name') }}</td></tr>
+            @endif
             <tr><th>name: </th><td><input type="text" name="name" value="{{ old('name') }}"></td></tr>
+
+            @if($errors->has('mail'))
+            <tr><th>ERRORRR</th><td>{{ $errors->first('mail') }}</td></tr>
+            @endif
             <tr><th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></tr>
+
+            @if($errors->has('age'))
+            <tr><th>ERRORRR</th><td>{{ $errors->first('age') }}</td></tr>
+            @endif
             <tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></tr>
             <tr><th></th><td><input type="submit" name="send"></td></tr>
         </table>
