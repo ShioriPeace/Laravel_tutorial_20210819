5ddc849ea589493c3fae848cc28fff14baee43d1
ShioriPeace <nkym_shiori.223@icloud.com>
2021-09-02 07:50:31 +0900

    正しくない値が入った時、エラーメッセージを表示するようにした

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,12 +9,21 @@
 
 @section('content')
     <p>{{ $msg }}</p>
+    @if(count($errors) > 0)
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     <form action="/hello" method="POST">
         <table>
             @csrf
-            <tr><th>name: </th><td><input type="text" name="name"></td></tr>
-            <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
-            <tr><th>age: </th><td><input type="text" name="age"></td></tr>
+            <tr><th>name: </th><td><input type="text" name="name" value="{{ old('name') }}"></td></tr>
+            <tr><th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></tr>
+            <tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></tr>
             <tr><th></th><td><input type="submit" name="send"></td></tr>
         </table>
     </form>
