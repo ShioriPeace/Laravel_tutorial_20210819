0357144b86392a59c00764f07d3b5c21be7e5a60
ShioriPeace <nkym_shiori.223@icloud.com>
2021-09-03 07:53:29 +0900

    フォームリクエストの中でバリデーションを設定したあと、クエリー文字列にバリデータを適用した

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,24 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
+use Illuminate\Support\Facades\Validator;
 
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index', ['msg'=>'フォームを入力']);
+        $validator = Validator::make($request->query(), [
+            'id' => 'required',
+            'pass' => 'required',
+        ]);
+
+        if($validator->fails()){
+            $msg = 'クエリーに問題があります';
+        }else{
+            $msg = 'ID/PASSを受け付けました。フォームを入力してください';
+        }
+
+        return view('hello.index', ['msg'=>$msg, ]);
     }
 
     public function post(Request $request)
     {
-        $validate_rule = [
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
+            'age' => 'numeric|between:0,50'
+        ]);
 
-        $this->validate($request, $validate_rule);
+        if($validator->fails()){
+            return redirect('/hello')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
---
+++ app/Http/Requests/HelloRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HelloRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if ($this->path() == 'hello')
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,50'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '名前は必ず入力してください',
+            'mail.email' => 'メールアドレスが必要です',
+            'age.numeric' => '年齢は整数で入力してください',
+            'age.between' => '年齢は、0~150の間で入力してください'
+        ];
+    }
+}
