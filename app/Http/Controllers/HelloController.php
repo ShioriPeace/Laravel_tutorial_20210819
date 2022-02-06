0089cbff61af93f84f39e3c8df6ee34c05e3a7ee
shioripeace <nkym_shiori.223@icloud.com>
2022-02-06 20:38:10 +0900

    whereRowを使った

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -19,9 +19,13 @@ class HelloController extends Controller
 
     public function show(Request $request)
     {
-        $id = $request->id;
-        $item = DB::table('people2')->where('id', $id)->first();
-        return view('hello.show', ['item' => $item]);
+        $min = $request->min;
+        $max = $request->max;
+
+        $items = DB::table('people2')
+        ->whereRaw('age >= ? and age <= ?',
+        [$min, $max])->get();
+        return view('hello.show', ['items' => $items]);
     }
 
     public function post(Request $request)
--- resources/views/hello/show.blade.php
+++ resources/views/hello/show.blade.php
@@ -8,13 +8,18 @@
 @endsection
 
 @section('content')
-        <table>
-            @csrf
-            <tr><th>id: </th><td>{{ $item->id }}</td></tr>
-            <tr><th>name: </th><td>{{ $item->name }}</td></tr>
-            <tr><th>mail: </th><td>{{ $item->mail }}</td></tr>
-            <tr><th>age: </th><td>{{ $item->age }}</td></tr>
+    @if($items != null)
+        @foreach($items as $item)
+        <table width="400px">
+            <tr>
+                <th width="50px">id:</th>
+                <td width="50px">{{ $item->id }}</td>
+                <th width="50px">name:</th>
+                <td width="50px">{{ $item->name }}</td>
+            </tr>
         </table>
+        @endforeach
+    @endif
 @endsection
 
 @section('footer')
