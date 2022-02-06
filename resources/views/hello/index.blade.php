1732e922154663d4b90bd63b225065cf2c52021d
shioripeace <nkym_shiori.223@icloud.com>
2022-02-06 13:41:52 +0900

    CRUDの実装をした（5-2完）

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,23 +8,16 @@
 @endsection
 
 @section('content')
-    <p>{{ $msg }}</p>
-    @if(count($errors) > 0)
-    <p>入力に問題あり！！再入力してください</p>
-    @endif
-    <form action="/hello" method="POST">
-        <table>
-            @csrf
-            @if ($errors->has('msg'))
-            <tr><th>ERROR</th><td>{{$error->first('msg') }}</td></tr>
-            @endif
-
-            <tr><th>Message: </th><td><input type="text" name="msg"
-                value="{{ old('msg') }}"></td></tr>
-
-            <tr><th></th><td><input type="submit" value="send"></td></tr>
-        </table>
-    </form>
+    <table>
+        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+        @foreach ($items as $item)
+            <tr>
+                <td>{{ $item->name }}</td>
+                <td>{{ $item->mail }}</td>
+                <td>{{ $item->age }}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
 
 @section('footer')
